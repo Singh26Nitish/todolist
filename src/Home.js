1c1e86ac95c1c75ac938eb1f59c0ca4c5424d361
Nitish Singh <nkushwaha791@gmail.com>
2023-11-17 14:01:34 +0530

    basic functions done

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function Home(){
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState('');
+
+    const addTask = () => {
+        if(task.trim() !== ''){
+            setTasks([...tasks, task])
+            setTask('')
+        }
+    }
+
+    const deleteTask = (index) => {
+        const updateTasks = [...tasks]
+        updateTasks.splice(index, 1)
+        setTasks(updateTasks)
+    }
+
+    
     return(
         <>
-        
+            <div>
+
+            </div>
         </>
     )
 }
